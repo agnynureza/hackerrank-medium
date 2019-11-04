09e8043dff616da0ed217812439a3a1e927780f4
agnynureza <rezaagny@gmail.com>
2019-11-04 13:15:45 +0700

    erase function

--- magic-square.js
+++ magic-square.js
@@ -124,7 +124,3 @@ function formingMagicSquare(s) {
 }
 
 console.log(formingMagicSquare([[4, 9, 2], [3, 5, 7], [8, 1, 5]]))
-// formingMagicSquare()
-// formingMagicSquare()
-// formingMagicSquare()
-
